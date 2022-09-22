7ae7f1b21a2352ce327b8dcfe5f394498cf9fae1
Jordan Smyth <jordansmyth@VPMX4YQ5M6.local>
2022-09-22 10:57:25 +0100

    added specific ordering API views to allow simulateous ordering and searching

--- trainers/urls.py
+++ trainers/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import ClubList, TrainerList, TrainerDetail, WorkshopList
+from .views import *
 
 urlpatterns = [
-    path('', TrainerList.as_view()),
+    path('list', TrainerList.as_view()),
+    path('list-trained-desc', LastTrainedDescendingTrainerList.as_view()),
+    path('list-trained-asc', LastTrainedAscendingTrainerList.as_view()),
     path('<int:pk>/', TrainerDetail.as_view()),
     path('workshop-list', WorkshopList.as_view()),
+    path('workshop-list-exp-desc', ExpDescendingWorkshopList.as_view()),
+    path('workshop-list-exp-asc', ExpAscendingWorkshopList.as_view()),
+    path('workshop-list-trained-asc', LastTrainedAscendingWorkshopList.as_view()),
+    path('workshop-list-trained-desc', LastTrainedDescendingWorkshopList.as_view()),
     path('clubs', ClubList.as_view()),
 ]
--- trainers/views.py
+++ trainers/views.py
@@ -11,9 +11,28 @@ class TrainerList(ListCreateAPIView):
     queryset = Trainer.objects.all()
     serializer_class = TrainerSerializer
     filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['ANDi_level']
+    search_fields = ['name', 'company_role', 'club__name' ]
+    ordering_fields = ['last_trained']
 
-#! Specific View of each Trainer - doesn't currently show which workshops they have experience_levels in (needs to)
+#! Pre-determined ordering for last_trained to allow search simultaneously
+class LastTrainedDescendingTrainerList(ListCreateAPIView):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+    filter_backends = [filters.OrderingFilter]
+    search_fields = ['name', 'company_role', 'club__name' ]
+    ordering_fields = ['last_trained']
+    ordering = ['-last_trained']
+
+class LastTrainedAscendingTrainerList(ListCreateAPIView):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+    filter_backends = [filters.OrderingFilter]
+    search_fields = ['name', 'company_role', 'club__name' ]
+    ordering_fields = ['last_trained']
+    ordering = ['last_trained']
+
+
+#! Specific View of each Trainer
 class TrainerDetail(RetrieveUpdateDestroyAPIView):
     queryset = Trainer.objects.all()
     serializer_class = PopulatedTrainerSerializer
@@ -24,7 +43,43 @@ class WorkshopList(ListCreateAPIView):
     serializer_class = Workshop_DetailsSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['workshop_name', 'experience_level', 'trainer__company_role', 'trainer__name']
-    ordering_fields = ['experience_level', 'trainer__ANDi_level']
+    ordering_fields = ['experience_level']
+    ordering = ['-experience_level']
+
+
+#! Specific views for pre-determined ordering while allowing searches
+class ExpDescendingWorkshopList(ListCreateAPIView):
+    queryset = Workshop_Details.objects.all()
+    serializer_class = Workshop_DetailsSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['workshop_name', 'experience_level', 'trainer__company_role', 'trainer__name']
+    ordering_fields = ['experience_level']
+    ordering = ['-experience_level']
+
+class ExpAscendingWorkshopList(ListCreateAPIView):
+    queryset = Workshop_Details.objects.all()
+    serializer_class = Workshop_DetailsSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['workshop_name', 'experience_level', 'trainer__company_role', 'trainer__name']
+    ordering_fields = ['experience_level']
+    ordering = ['experience_level']
+
+class LastTrainedDescendingWorkshopList(ListCreateAPIView):
+    queryset = Workshop_Details.objects.all()
+    serializer_class = Workshop_DetailsSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['workshop_name', 'experience_level', 'trainer__company_role', 'trainer__name']
+    ordering_fields = ['trainer__last_trained']
+    ordering = ['trainer__last_trained']
+
+class LastTrainedAscendingWorkshopList(ListCreateAPIView):
+    queryset = Workshop_Details.objects.all()
+    serializer_class = Workshop_DetailsSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['workshop_name', 'experience_level', 'trainer__company_role', 'trainer__name']
+    ordering_fields = ['trainer__last_trained']
+    ordering = ['-trainer__last_trained']
+
 
 
 #! List of all Clubs with nested trainers
@@ -33,4 +88,3 @@ class ClubList(ListCreateAPIView):
     serializer_class = PopulatedClubSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['name', 'trainers__last_trained', 'trainers__name']
-    ordering_fields = ['trainers__last_trained']
